23c8518108c0723326786ec5931134ec5f1f39c3
Pierre-Adrien <pabuisson@gmail.com>
2016-09-21 13:58:16 +0200

    CHORE Refactor authentication code
    * CHORE Refactored authentication to remove some duplicate code, and added storage of
      data (access token most importantly) into local storage.
    * Replaced var declarations with ES6 const/let
    * FEATURE Trigger authentication from a button in the popup
    * CHORE Use promise to resolve "isAuthenticated"

--- src/background.js
+++ src/background.js
@@ -1,9 +1,33 @@
+// TODO: Use a button in the popup to trigger the authentication process
+// TODO: Avoid the loading of a given page (redirectIntermediate variable)
+
+
 // TODO Replace this by an "internal" page with its own JS...
+const redirectIntermediate = 'http://blog.pabuisson.com';
 // var redirectIntermediate = chrome.extension.getURL('src/test.html');
-var redirectIntermediate = 'http://blog.pabuisson.com';
-var redirectAuthFinished = 'https://getpocket.com';
-var consumerKey = '54622-1b67c0d0aa575fd165c31d2f';
-var requestToken = undefined;
+const redirectAuthFinished = 'https://getpocket.com';
+const consumerKey = '54622-1b67c0d0aa575fd165c31d2f';
+
+
+prepareRequest = function( url, action ) {
+  var request = new XMLHttpRequest();
+
+  if( url && ( action == 'GET' || action == 'POST' ) ) {
+    request.open( action, url, true );
+  }
+
+  //Send the proper header information along with the request
+  request.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  request.setRequestHeader("X-Accept", "application/json");
+
+  // There was a connection error of some sort
+  request.onerror = function() {
+    console.error('error while reaching the server!');
+    console.error(request);
+  };
+
+  return request;
+};
 
 
 tabCallback = function( tabId, changeInfo, updatedTab ) {
@@ -11,52 +35,40 @@ tabCallback = function( tabId, changeInfo, updatedTab ) {
   if (changeInfo.status == 'complete' && updatedTab.url.indexOf(redirectIntermediate) === 0) {
     browser.tabs.remove( tabId );
 
-    var request = new XMLHttpRequest();
-    request.open('POST', 'https://getpocket.com/v3/oauth/authorize', true);
-
-    //Send the proper header information along with the request
-    request.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    request.setRequestHeader("X-Accept", "application/json");
+    let request = prepareRequest( 'https://getpocket.com/v3/oauth/authorize', 'POST' );
 
     request.onload = function() {
-      console.log('request 2 onload');
       if( this.status >= 200 && this.status < 400) {
-        var response = JSON.parse( this.response );
-        var username = response.username;
-        var access_token = response.access_token;
+        const response = JSON.parse( this.response );
+        const username = response.username;
+        const access_token = response.access_token;
 
-        console.log(`username: ${ username } // access_token: ${ access_token }`);
-        // TODO: Store the access_token somewhere
-        // NOTE: Need the access_token + consumer_key for further requests
+        // Store the access_token in local storage
+        browser.storage.local.set({ access_token: access_token });
+        browser.storage.local.set({ username: username });
+
         // TODO: Display something to the user?
       }
     };
 
-    request.onerror = function() {
-      // There was a connection error of some sort
-      console.error('error while reaching the server!');
-      console.error(request);
-    };
-
-    request.send(JSON.stringify({ consumer_key: consumerKey, code: requestToken }));
+    browser.storage.local.get( 'requestToken', function( data ) {
+      requestParams = JSON.stringify({ consumer_key: consumerKey, code: data.requestToken });
+      request.send( requestParams );
+    });
   }
 };
 
-obtainConsumerKey = function() {
-  var request = new XMLHttpRequest();
-  request.open('POST', 'https://getpocket.com/v3/oauth/request', true);
-
-  //Send the proper header information along with the request
-  request.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-  request.setRequestHeader("X-Accept", "application/json");
+authenticate = function() {
+  let request = prepareRequest( 'https://getpocket.com/v3/oauth/request', 'POST' );
 
   request.onload = function() {
     if( this.status >= 200 && this.status < 400 ) {
-      // Success!
-      var response = JSON.parse( this.response );
-      requestToken = response.code;
+      const response = JSON.parse( this.response );
+      const requestToken = response.code;
 
-      var authorizeUrl = "https://getpocket.com/auth/authorize?request_token=" + requestToken + "&redirect_uri=" + redirectIntermediate;
+      browser.storage.local.set({ requestToken: requestToken });
+
+      const authorizeUrl = "https://getpocket.com/auth/authorize?request_token=" + requestToken + "&redirect_uri=" + redirectIntermediate;
       browser.tabs.create({ 'url': authorizeUrl }, function( tab ) {
         browser.tabs.onUpdated.addListener( tabCallback );
       });
@@ -66,16 +78,30 @@ obtainConsumerKey = function() {
     }
   };
 
-  request.onerror = function() {
-    // There was a connection error of some sort
-    console.error('error while reaching the server!');
-    console.error(request);
-  };
-
-  request.send(JSON.stringify({ consumer_key: consumerKey, redirect_uri: redirectAuthFinished }));
+  requestParams = JSON.stringify( { consumer_key: consumerKey, redirect_uri: redirectAuthFinished } )
+  request.send( requestParams );
 };
 
 
-console.log('--- background.js ----');
-obtainConsumerKey();
+isAuthenticated = function() {
+  let promise = new Promise( function( resolve, reject ) {
+    browser.storage.local.get('access_token').then( function(data) {
+      if( 'access_token' in data ) {
+        resolve( data.access_token );
+      } else {
+        reject();
+      }
+    });
+  });
 
+  return promise;
+}
+
+
+chrome.runtime.onMessage.addListener( function( message ) {
+  switch(message) {
+    case 'authenticate':
+      authenticate();
+      break;
+  }
+});
--- src/popup/list.js
+++ src/popup/list.js
@@ -1,3 +1,17 @@
-console.log('--- list.js ---');
-
 // JS that can be executed while the popup is displayed
+
+document.querySelector( '.authentication button' ).addEventListener( 'click', function() {
+  chrome.runtime.sendMessage('authenticate');
+});
+
+setUI = function() {
+  isAuthenticated().then( function( access_token ) {
+    document.querySelector( '.authentication' ).style.display = 'none';
+    document.querySelector( '.authenticated'  ).style.display = 'block';
+  }, function( error ) {
+    document.querySelector( '.authentication' ).style.display = 'block';
+    document.querySelector( '.authenticated'  ).style.display = 'none';
+  });
+}
+
+setUI();
