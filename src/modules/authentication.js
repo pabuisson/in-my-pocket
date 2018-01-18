4d18a99cd2d5c4f92e725a3da248a3924ce027be
Pierre-Adrien <pabuisson@gmail.com>
2018-01-18 22:09:07 +0100

    REFACTOR Extract all authentication stuff to module
    REF: [#195](https://trello.com/c/kKWyH6pc)
    
    Details
    -------
    * REFACTOR: merged AuthenticationProcess from background into Authentication module
    * REFACTOR: promisified authentication code
    * REFACTOR: stopped storing requestToken (only needed during auth process)
    * REFACTOR: moved consumerKey to constants module instead of hardcoding in 2 places
    * REFACTOR: removed the old `RequestBuilder` module, only left `Request` class

--- src/modules/authentication.js
+++ src/modules/authentication.js
@@ -1,18 +1,83 @@
 "use strict";
 
 import Logger from './logger.js';
+import Request from './request_builder.js';
+import { consumerKey } from './constants.js';
 
 
 // -------------------------------------
 
 
-// TODO Add all the authentication logic in this module
+// TODO: After the opening of the callback tab, reactivate the tab where the
+//      user was before for a less disturbing behaviour
 
 var Authentication = ( function() {
+  // TODO: Replace by an "internal" page with its own JS
+  const redirectIntermediate = 'https://oauth.pabuisson.com';
+  const redirectAuthFinished = 'https://oauth.pabuisson.com';
+
+  function authenticateStep2( requestToken ) {
+    Logger.log('(Authentication.authenticateStep2) Request API authorize to finish authentication');
+    const requestParams = {
+      consumer_key: consumerKey,
+      code: requestToken
+    };
+
+    return new Request( 'POST', 'https://getpocket.com/v3/oauth/authorize', requestParams )
+      .fetch()
+      .then( function( response ) {
+        Logger.log('(Authentication.authenticateStep2) Got the access_token, we are all good!');
+        const username = response.username;
+        const access_token = response.access_token;
+
+        // Store the access_token in local storage
+        browser.storage.local.set({ access_token: access_token });
+        browser.storage.local.set({ username: username });
+
+        return true;
+      });
+  }
+
   return {
+    authenticate: function() {
+      Logger.log('(Authentication.authenticate) Starting the authentication process');
+      let promise = new Promise( (resolve, reject) => {
+        const requestParams = {
+          consumer_key: consumerKey,
+          redirect_uri: redirectAuthFinished
+        };
+
+        Logger.log('(Authentication.authenticate) Requesting the requestToken');
+        new Request( 'POST', 'https://getpocket.com/v3/oauth/request', requestParams )
+          .fetch()
+          .then( ( response ) => {
+            Logger.log('(Authentication.authenticate) Got the requestToken, open an authorize tab')
+            const requestToken = response.code;
+
+            const authorizeUrl = "https://getpocket.com/auth/authorize?request_token=" + requestToken + "&redirect_uri=" + redirectIntermediate;
+            browser.tabs.create({ 'url': authorizeUrl }, ( tab ) => {
+              browser.tabs.onUpdated.addListener( ( tabId, changeInfo, updatedTab ) => {
+                // callback url has been loaded
+                if (changeInfo.status == 'complete' && updatedTab.url.indexOf(redirectIntermediate) === 0) {
+                  Logger.log('(Authentication.authenticate) Authorize tab has been loaded correctly');
+                  browser.tabs.remove( tabId );
+
+                  authenticateStep2( requestToken )
+                    .then( resolve )
+                    .catch( reject );
+                }
+              });
+            });
+          });
+      });
+
+      return promise;
+    },
+
+
     isAuthenticated: function() {
-      let promise = new Promise( function( resolve, reject ) {
-        browser.storage.local.get('access_token').then( function( { access_token } ) {
+      let promise = new Promise( ( resolve, reject ) => {
+        browser.storage.local.get('access_token').then( ({ access_token }) => {
           if( access_token ) {
             Logger.log( '(Authentication.isAuthenticated) access_token present, user is authenticated' );
             resolve( access_token );
@@ -27,5 +92,12 @@ var Authentication = ( function() {
     }
   }
 })();
+
+
+
+
+var AuthenticationProcess = ( function() {
+})();
+
 
 export default Authentication;
