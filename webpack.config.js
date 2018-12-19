01f56061c6864e5ebb92c7b915e5e932e14f7ac2
Pierre-Adrien <pabuisson@gmail.com>
2018-12-19 21:47:47 +0100

    CHORE: updated webpack build and package.json build scripts to deal with two         different manifest files and two different builds (firefox/chrome)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin    = require('copy-webpack-plugin');
 
-module.exports = {
+var base = {
   context: __dirname + '/src',
   mode: 'production',
   entry: {
@@ -14,10 +14,7 @@ module.exports = {
     'background/context_menus' : './background/context_menus.js',
     'background/uninstall'     : './background/uninstall.js'
   },
-  output: {
-    path: __dirname + '/build',
-    filename: '[name].js'
-  },
+  // output: {}
   module: {
     rules: [{
       test: /\.html$/,
@@ -43,12 +40,44 @@ module.exports = {
         "sass-loader" // compiles Sass to CSS, using Node Sass by default
       ]
     }]
+  }
+  // plugins: []
+};
+
+// ---------------------------------------------------------------------------
+
+var firefoxSpecific = {
+  output: {
+    path: __dirname + '/build/firefox',
+    filename: '[name].js'
   },
   plugins: [
     new MiniCssExtractPlugin(),
     new CopyWebpackPlugin([
       { from: 'assets/', to: 'assets/',  ignore: [ '.DS_Store' ] },
-      { from: 'manifest.json', to: 'manifest.json' }
+      { from: 'manifest_firefox.json', to: 'manifest.json' }
     ])
   ]
 };
+
+var chromeSpecific = {
+  output: {
+    path: __dirname + '/build/chrome',
+    filename: '[name].js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin([
+      { from: 'assets/', to: 'assets/',  ignore: [ '.DS_Store' ] },
+      { from: 'manifest_chrome.json', to: 'manifest.json' },
+      { from: '../node_modules/webextension-polyfill/dist/browser-polyfill.min.js', to: 'assets/javascripts/' }
+    ])
+  ]
+};
+
+var firefox = Object.assign({}, base, firefoxSpecific);
+var chrome  = Object.assign({}, base, chromeSpecific);
+
+// ---------------------------------------------------------------------------
+
+module.exports = [ firefox, chrome ];
