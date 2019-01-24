82ee68002b54111bafefae6099ccce247f50ae11
Pierre-Adrien <pabuisson@gmail.com>
2019-01-24 09:43:42 +0100

    FEATURE: add a spinning text to the badge when operation in progress
    REF: [#271](https://trello.com/c/tddeAjYR)
    
    Details
    -------
    * REFACTOR: Badge.flashError/Success could be greatly simplified
    * REFACTOR: Promisified the badge update, so that I can update the UI in sync with the badge update
    
    Others
    ------
    * CHORE: Added DS_Store to gitignore

--- src/modules/badge.js
+++ src/modules/badge.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import Logger from './logger.js';
 import Settings from './settings.js';
 import Utility from './utility.js';
 
@@ -7,17 +8,35 @@ import Utility from './utility.js';
 // -------------------------------------
 
 
-var Badge = ( function() {
-  let defaultBackgroundColor = '#333';
-  let successBackgroundColor = '#12bc00'; // ff-green-60
-  let errorBackgroundColor   = '#ff0039'; // ff-red-50
+const Badge = ( function() {
+  const defaultBackgroundColor = '#333';
+  const successBackgroundColor = '#12bc00'; // ff-green-60
+  const errorBackgroundColor   = '#ff0039'; // ff-red-50
+  const spinnerChars           = '⌟⌞⌜⌝';
+  let spinnerCurrentChar;
+  let spinnerIntervalID;
 
-  function itemsNumbers( items ) {
-    if( items && Object.keys( items ).length > 0 ) {
-      return Object.keys( items ).length;
+  function itemsNumbers(items) {
+    if( items && Object.keys(items).length > 0 ) {
+      return Object.keys(items).length;
     } else {
       return '';
     }
+  }
+
+  function stopLoadingSpinner() {
+    Logger.log('(stopLoadingSpinner)');
+
+    if(spinnerIntervalID) {
+      clearInterval(spinnerIntervalID);
+      spinnerIntervalID = null;
+    }
+  }
+
+  function getNextSpinnerChar() {
+    const nextCharIndex     = ( spinnerChars.indexOf(spinnerCurrentChar) + 1 ) % spinnerChars.length;
+    const nextSpinnerChar = spinnerChars[nextCharIndex];
+    return nextSpinnerChar;
   }
 
   return {
@@ -25,58 +44,70 @@ var Badge = ( function() {
       browser.browserAction.setBadgeText({ text: '' });
     },
 
-    updateCount: function( items ) {
-      Settings.init().then( function() {
-        let showBadge = Settings.get( 'showBadge' );
-        if( showBadge === true ) {
-          browser.browserAction.setBadgeBackgroundColor({ color: defaultBackgroundColor });
+    updateCount: function(items) {
+      return new Promise( resolve => {
+        stopLoadingSpinner();
+        Settings.init().then( function() {
+          const showBadge = Settings.get('showBadge');
+          if(showBadge === true) {
+            browser.browserAction.setBadgeBackgroundColor({ color: defaultBackgroundColor });
 
-          if( items ) {
-            let itemsCount = itemsNumbers( items );
-            browser.browserAction.setBadgeText({ text: itemsCount.toString() });
-          } else {
-            browser.storage.local.get('items').then( ({ items }) => {
-              let itemsCollection = Utility.parseJson( items ) || [];
-              let itemsCount = itemsNumbers( itemsCollection );
+            if(items) {
+              const itemsCount = itemsNumbers(items);
               browser.browserAction.setBadgeText({ text: itemsCount.toString() });
-            });
+              resolve();
+            } else {
+              browser.storage.local.get('items').then( ({ items }) => {
+                const itemsCollection = Utility.parseJson(items) || [];
+                const itemsCount = itemsNumbers(itemsCollection);
+                browser.browserAction.setBadgeText({ text: itemsCount.toString() });
+                resolve();
+              });
+            }
+          } else {
+            // If showBadge != true, we hide the count badge
+            Badge.hide();
+            resolve();
           }
-        } else {
-          // If showBadge != true, we hide the count badge
-          Badge.hide();
-        }
+        });
       });
     },
 
+    startLoadingSpinner: function() {
+      if(!spinnerIntervalID) {
+        Logger.log('(startLoadingSpinner) will start the spinner now');
+        spinnerIntervalID = setInterval(() => {
+          spinnerCurrentChar = getNextSpinnerChar();
+          browser.browserAction.setBadgeText({ text: spinnerCurrentChar });
+        }, 150);
+      }
+    },
+
     flashSuccess: function() {
-      browser.storage.local.get('items').then( ({ items }) => {
-        let parsedItems = Utility.parseJson( items ) || [];
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          stopLoadingSpinner();
 
-        browser.browserAction.setBadgeText({ text: '✓' });
-        browser.browserAction.setBadgeBackgroundColor({ color: successBackgroundColor });
+          browser.browserAction.setBadgeText({ text: '✓' });
+          browser.browserAction.setBadgeBackgroundColor({ color: successBackgroundColor });
 
-        setTimeout( function() {
-          // Reset the color to default
-          browser.browserAction.setBadgeBackgroundColor({ color: defaultBackgroundColor });
-          // Update the badge if needed
-          Badge.updateCount( parsedItems );
-        }, 2000);
+          setTimeout(Badge.updateCount, 1500);
+          resolve();
+        }, 1000);
       });
     },
 
     flashError: function() {
-      browser.storage.local.get('items').then( ({ items }) => {
-        let parsedItems = Utility.parseJson( items ) || [];
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          stopLoadingSpinner();
 
-        browser.browserAction.setBadgeText({ text: '!' });
-        browser.browserAction.setBadgeBackgroundColor({ color: errorBackgroundColor });
+          browser.browserAction.setBadgeText({ text: '!' });
+          browser.browserAction.setBadgeBackgroundColor({ color: errorBackgroundColor });
 
-        setTimeout( function() {
-          // Reset the color to default
-          browser.browserAction.setBadgeBackgroundColor({ color: defaultBackgroundColor });
-          // Update the badge if needed
-          Badge.updateCount( parsedItems );
-        }, 2000);
+          setTimeout(Badge.updateCount, 1500);
+          resolve();
+        }, 1000);
       });
     }
   };
