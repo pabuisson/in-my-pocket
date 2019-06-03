b166952c771ae627ae3f893f4a7db93832c58f6e
zakius@windowslive.com <jaroslaw.jarosik@outlook.com>
2019-06-03 22:22:37 +0200

    Avoid mutating parameter

--- src/modules/items.js
+++ src/modules/items.js
@@ -233,9 +233,9 @@ const Items = ( function() {
       removeItem(itemId, 'delete');
     },
 
-    open: function(itemId, openInNewTab = false) {
+    open: function(itemId, forceNewTab = false) {
       Settings.init().then( () => {
-        openInNewTab      = Settings.get('openInNewTab') || openInNewTab;
+        const openInNewTab      = Settings.get('openInNewTab') || forceNewTab;
         const archiveWhenOpened = Settings.get('archiveWhenOpened');
 
         browser.storage.local.get('items').then( ({ items }) => {
