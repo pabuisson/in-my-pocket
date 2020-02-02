b953cac196b27ea6eca8b75178f1b9982a0b00d6
Pierre-Adrien <pabuisson@gmail.com>
2020-02-02 22:16:42 +0100

    Add UI items (ugly, but it's there, that's a start)

--- src/modules/popup_top_actions.js
+++ src/modules/popup_top_actions.js
@@ -1,6 +1,8 @@
 "use strict";
 
 import PopupMainLoader from './popup_main_loader.js';
+import PopupTopFilter from './popup_top_filter';
+import { FavoriteFilterState } from './constants.js'
 
 // ----------------
 
@@ -10,15 +12,34 @@ const PopupTopActions = ( function() {
   const openSettingsButton   = document.querySelector('.open-settings');
   const readRandomItemButton = document.querySelector('.random-item');
   const retrieveItemsButton  = document.querySelector('.retrieve-items');
+  const filterFavoriteIcon   = document.querySelector('.filter-faved');
+  const filterFavoriteIconOn = document.querySelector('.filter-faved-on');
+  const filterFavoriteIconOff = document.querySelector('.filter-faved-off');
 
   return {
     setupEventListeners: function() {
-      retrieveItemsButton.addEventListener( 'click', function() {
+      retrieveItemsButton.addEventListener('click', function() {
         PopupMainLoader.enable();
         browser.runtime.sendMessage({ action: 'retrieve-items', force: true });
       });
 
-      addCurrentPageButton.addEventListener( 'click', function() {
+      filterFavoriteIcon.addEventListener('click', function() {
+        const currentFilter = filterItemsInput.value;
+        const currentFilterState = PopupTopFilter.getFavoriteFilterState();
+
+        if(currentFilterState == FavoriteFilterState.ON) {
+          PopupTopFilter.setValue(currentFilter.replace('is:faved', 'is:unfaved').trim());
+          filterFavoriteIcon.style.color = 'red';
+        } else if(currentFilterState == FavoriteFilterState.OFF) {
+          PopupTopFilter.setValue(currentFilter.replace('is:unfaved', '').trim());
+          filterFavoriteIcon.style.color = 'grey';
+        } else if(currentFilterState == FavoriteFilterState.UNSET) {
+          PopupTopFilter.setValue(`${currentFilter} is:faved`);
+          filterFavoriteIcon.style.color = 'lime';
+        }
+      });
+
+      addCurrentPageButton.addEventListener('click', function() {
         PopupMainLoader.enable();
         browser.tabs.query({ active: true, currentWindow: true }).then( ([currentTab]) => {
           const currentUrl   = currentTab.url;
@@ -28,14 +49,14 @@ const PopupTopActions = ( function() {
         });
       });
 
-      readRandomItemButton.addEventListener( 'click', () => {
+      readRandomItemButton.addEventListener('click', () => {
         browser.runtime.sendMessage({
           action: 'random-item',
           query: filterItemsInput.value
         });
       });
 
-      openSettingsButton.addEventListener( 'click', () => {
+      openSettingsButton.addEventListener('click', () => {
         // Close the popup in a setTimeout, otherwise the "openOptionsPage" part
         // has no valid "context" to be executed (and closing the popup in the promise
         // feels a lot slower)
--- src/modules/popup_top_filter.js
+++ src/modules/popup_top_filter.js
@@ -4,6 +4,7 @@ import Logger          from './logger.js';
 import PopupUI         from './popup_ui.js';
 import PopupMainLoader from './popup_main_loader.js';
 import Utility         from './utility.js';
+import { FavoriteFilterState } from './constants.js';
 
 // ----------------
 
@@ -11,10 +12,40 @@ const PopupTopFilter = ( function() {
   const filterItemsInput     = document.querySelector('.filter-items');
   const clearSearchBoxButton = document.querySelector('.clear-search-box');
 
+  function onFilterChanged() {
+    const query = filterItemsInput.value.toLowerCase();
+    if(query !== '') {
+      PopupMainLoader.enable();
+      clearSearchBoxButton.classList.remove('hidden');
+    } else {
+      clearSearchBoxButton.classList.add('hidden');
+    }
+
+    // Save query to localStorage 'display' variable
+    browser.storage.local.get('display').then( ({ display }) => {
+      const parsedDisplay  = Utility.parseJson(display) || {};
+      const displayOptions = Object.assign({}, parsedDisplay, { query: query });
+      browser.storage.local.set({ display: JSON.stringify(displayOptions) });
+    });
+
+    // Draw the items lists
+    Logger.log(`(onFilterChanged) will draw list with query=${query}`);
+    PopupUI.drawList({ page: 1, query: query });
+    PopupMainLoader.disable(true);
+  }
+
   return {
+    getFavoriteFilterState: function() {
+      const currentFilter = filterItemsInput.value;
+      if(currentFilter.includes('is:faved')) return FavoriteFilterState.ON;
+      if(currentFilter.includes('is:unfaved')) return FavoriteFilterState.OFF;
+      return FavoriteFilterState.UNSET;
+    },
+
     setValue: function(query) {
       Logger.log(`(PopupTopFilter.setValue) set search query to ${query}`);
       filterItemsInput.value = query || '';
+      onFilterChanged();
 
       // Show/Hide the clear search button depending on the restored query value
       if(filterItemsInput.value === '') {
@@ -22,6 +53,7 @@ const PopupTopFilter = ( function() {
       } else {
         clearSearchBoxButton.classList.remove('hidden');
       }
+
     },
 
     focusSearchField: function() {
@@ -37,29 +69,8 @@ const PopupTopFilter = ( function() {
         PopupTopFilter.focusSearchField();
       });
 
-      const debouncedFilterEventHandler = Utility.debounce( function() {
-        const query = this.value.toLowerCase();
-        if(query !== '') {
-          PopupMainLoader.enable();
-          clearSearchBoxButton.classList.remove('hidden');
-        } else {
-          clearSearchBoxButton.classList.add('hidden');
-        }
-
-        // Save query to localStorage 'display' variable
-        browser.storage.local.get('display').then( ({ display }) => {
-          const parsedDisplay  = Utility.parseJson(display) || {};
-          const displayOptions = Object.assign({}, parsedDisplay, { query: query });
-          browser.storage.local.set({ display: JSON.stringify(displayOptions) });
-        });
-
-        // Draw the items lists
-        Logger.log(`(debouncedFilterEventHandler) will draw list with query=${query}`);
-        PopupUI.drawList({ page: 1, query: query });
-        PopupMainLoader.disable(true);
-      }, 200);
-
-      filterItemsInput.addEventListener('keyup', debouncedFilterEventHandler);
+      const debouncedFilterEventHandler = Utility.debounce(onFilterChanged, 200);
+      filterItemsInput.addEventListener('input', debouncedFilterEventHandler);
     }
   };
 })();
