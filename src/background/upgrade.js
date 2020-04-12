590a4bbd504cc09f4587ca3eab6d58b49f14ae77
Pierre-Adrien <pabuisson@gmail.com>
2020-04-12 22:36:07 +0200

    Enrich VersionManager to contain resync and show notification checks

--- src/background/upgrade.js
+++ src/background/upgrade.js
@@ -1,48 +1,48 @@
 "use strict";
 
 import Logger  from '../modules/logger.js';
-import { VersionManager, VERSION_LEVELS } from '../modules/version_manager.js';
+import { VersionManager } from '../modules/version_manager.js';
 
 const installNotificationId = '0001';
 const upgradeNotificationId = '0002';
 
 
-function mustShowUpdateNotification( details ) {
-  const isUpdate = (details.reason == 'update');
-  const isMajorOrMinorUpdate = VersionManager.isUpdate(details.previousVersion, VERSION_LEVELS.MINOR);
-
-  return isUpdate && isMajorOrMinorUpdate;
-}
-
-browser.runtime.onInstalled.addListener( details => {
-  if( details.reason == "install" ) {
-    Logger.log("Fresh install! Welcome on board :)");
-    browser.notifications.create( installNotificationId, {
+browser.runtime.onInstalled.addListener(details => {
+  if(details.reason === 'install') {
+    Logger.log('Fresh install! Welcome on board :)');
+    browser.notifications.create(installNotificationId, {
       type: 'basic',
       title: 'Yay, welcome to In My Pocket 🎉',
-      message: "Click this message to know more about the addon!",
+      message: 'Click this message to learn more about the addon!',
     });
-  } else if( mustShowUpdateNotification(details) ) {
-    Logger.log("IMP has been upgraded from " + details.previousVersion + " !");
-    browser.notifications.create( upgradeNotificationId, {
+  } else if(VersionManager.mustShowUpdateNotification(details)) {
+    // Upgrade notification
+    Logger.log(`IMP has been upgraded from ${details.previousVersion} !`);
+    browser.notifications.create(upgradeNotificationId, {
       type: 'basic',
       title: 'Yay, In My Pocket has been upgraded 🎉',
-      message: "Click this message to see what's new!",
+      message: "Click this message to learn what's new!",
     });
+
+    // Check if need to trigger a full resync
+    browser.storage.local.get(['access_token', 'lastFullSyncAtVersion'])
+      .then(({ access_token, lastFullSyncAtVersion }) => {
+        if (access_token && VersionManager.mustTriggerFullResync(lastFullSyncAtVersion)) {
+          browser.runtime.sendMessage({ action: 'retrieve-items', force: true });
+        }
+      });
   }
 });
-
 
 browser.notifications.onClicked.addListener(notificationId => {
   switch(notificationId) {
     case installNotificationId:
-      browser.notifications.clear( notificationId );
+      browser.notifications.clear(notificationId);
       browser.tabs.create({ 'url': 'https://inmypocketaddon.com/faq.html?utm_source=addon&utm_medium=notification&utm_campaign=install' });
       break;
     case upgradeNotificationId:
-      browser.notifications.clear( notificationId );
+      browser.notifications.clear(notificationId);
       browser.tabs.create({ 'url': 'https://inmypocketaddon.com/changelog.html?utm_source=addon&utm_medium=notification&utm_campaign=upgrade' });
       break;
   }
 });
-
