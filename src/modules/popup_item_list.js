31da68ef08ce9617445aca483a520fad9af653ba
Pierre-Adrien Buisson <pierre-adrien.buisson@doctolib.com>
2020-06-06 12:05:08 +0200

    Favicon: HTTP -> HTTPS

--- src/modules/popup_item_list.js
+++ src/modules/popup_item_list.js
@@ -22,7 +22,7 @@ const PopupItemList = ( function() {
   }
 
   function faviconUrl(url) {
-    return "http://www.google.com/s2/favicons?domain_url=" + encodeURIComponent(url);
+    return "https://www.google.com/s2/favicons?domain_url=" + encodeURIComponent(url);
   }
 
   function formatUrl(url) {
