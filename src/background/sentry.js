651b3ec866d30b67ed9f8321da840b6c7006d5c8
Pierre-Adrien Buisson <pierre-adrien.buisson@doctolib.com>
2020-11-07 17:05:01 +0100

    Sentry: normalize paths for better sentry stacktrace

--- src/background/sentry.js
+++ src/background/sentry.js
@@ -4,6 +4,10 @@ import { VersionManager } from '../modules/version_manager.js';
 
 // --------------------------
 
+const normalizePath = (path) => {
+  return path.replace(/(webpack_require__@)?(moz|chrome)-extension:\/\/[^\/]+\//, '~/');
+};
+
 Settings.init().then(() => {
   Sentry.init({
     dsn: "https://1207d464a72f4c788936641a73b872a2@o153528.ingest.sentry.io/1206746",
@@ -12,9 +16,22 @@ Settings.init().then(() => {
     release: "in-my-pocket@" + VersionManager.getCurrentVersion(),
 
     beforeSend(event) {
-      if (Settings.get('bugReport')) {
-        return event;
+      if (!Settings.get('bugReport')) {
+        return null;
       }
+
+      if (event.culprit) {
+        event.culprit = normalizePath(event.culprit);
+      }
+
+      if (event.exception) {
+        event.exception.values[0].stacktrace.frames = event.exception.values[0].stacktrace.frames.map((frame) => {
+          frame.filename = normalizePath(frame.filename);
+          return frame;
+        });
+      }
+
+      return event;
     },
   });
 });
