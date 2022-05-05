ef8067d20d75cf8cfe48ca7b79f264957a7789c4
Pierre-Adrien Buisson <pabuisson@gmail.com>
2022-05-05 11:26:53 +0200

    Refacto: reduce duplication in popup tag edition

--- src/modules/popup_tag_edition.js
+++ src/modules/popup_tag_edition.js
@@ -3,18 +3,31 @@
 import Utility from "../modules/utility.js"
 
 const PopupTagEdition = (function () {
+  function getTagElements(eventTarget) {
+    const editedItem = Utility.getParent(eventTarget, ".item")
+    const tagElements = editedItem.querySelectorAll(".tags .tag")
+    return tagElements
+  }
+
+  function getNewTagField(eventTarget) {
+    const editedItem = Utility.getParent(eventTarget, ".item")
+    const newTagField = editedItem.querySelector(".new-tag")
+    return newTagField
+  }
+
+  function hasNoTags(eventTarget) {
+    return getTagElements(eventTarget).length === 0
+  }
+
   return {
     focusNextTag: function (ev) {
-      const editedItem = Utility.getParent(ev.target, ".item")
-      const tagElements = editedItem.querySelectorAll(".tags .tag")
+      if (hasNoTags(ev.target)) return
 
-      if (tagElements.length === 0) return
-
-      const focusedTagIndex = Array.from(tagElements).findIndex(
-        tag => tag == document.activeElement
-      )
+      const tagElements = getTagElements(ev.target)
+      const focusedTagIndex = Array.from(tagElements).findIndex(tag => tag == document.activeElement)
       const noTagFocused = focusedTagIndex === -1
       const lastTagFocused = focusedTagIndex === tagElements.length - 1
+
       if (noTagFocused || lastTagFocused) {
         tagElements[0].focus()
       } else {
@@ -23,16 +36,13 @@ const PopupTagEdition = (function () {
     },
 
     focusPreviousTag: function (ev) {
-      const editedItem = Utility.getParent(ev.target, ".item")
-      const tagElements = editedItem.querySelectorAll(".tags .tag")
+      if (hasNoTags(ev.target)) return
 
-      if (tagElements.length === 0) return
-
-      const focusedTagIndex = Array.from(tagElements).findIndex(
-        tag => tag == document.activeElement
-      )
+      const tagElements = getTagElements(ev.target)
+      const focusedTagIndex = Array.from(tagElements).findIndex(tag => tag == document.activeElement)
       const noTagFocused = focusedTagIndex === -1
       const firstTagFocused = focusedTagIndex === 0
+
       if (noTagFocused || firstTagFocused) {
         tagElements[tagElements.length - 1].focus()
       } else {
@@ -49,7 +59,7 @@ const PopupTagEdition = (function () {
       newTag.setAttribute("tabIndex", -1)
       newTag.textContent = ev.target.value
 
-      const newTagField = editedItem.querySelector(".new-tag")
+      const newTagField = getNewTagField(ev.target)
       newTagField.value = ""
       ev.target.dataset.previousValue = ""
 
@@ -57,33 +67,26 @@ const PopupTagEdition = (function () {
     },
 
     deleteLastTag: function (ev) {
-      const editedItem = Utility.getParent(ev.target, ".item")
-      const tagElements = editedItem.querySelectorAll(".tags .tag")
+      if (hasNoTags(ev.target)) return
 
-      if (tagElements.length === 0) return
-
+      const tagElements = getTagElements(ev.target)
       const lastTagElement = tagElements[tagElements.length - 1]
       lastTagElement.parentNode.removeChild(lastTagElement)
     },
 
     deleteFocusedTagAndFocusPreviousOne: function (ev) {
-      const editedItem = Utility.getParent(ev.target, ".item")
-      const tagElements = editedItem.querySelectorAll(".tags .tag")
+      if (hasNoTags(ev.target)) return
 
-      if (tagElements.length === 0) return
-
-      const focusedTagIndex = Array.from(tagElements).findIndex(
-        tag => tag == document.activeElement
-      )
-
+      const tagElements = getTagElements(ev.target)
+      const focusedTagIndex = Array.from(tagElements).findIndex(tag => tag == document.activeElement)
       const focusedElement = tagElements[focusedTagIndex]
       focusedElement.parentNode.removeChild(focusedElement)
 
-      // TODO: this piece of logic has nothing to do with deleting focused tag,
-      // but giving focus to next item. This should be moved out of this method
-      const deletedLastTag = tagElements.length === 1
+      // TODO: this piece of logic is about giving focus to next item and should be moved out of this method
+      const deletedLastTag = hasNoTags(ev.target)
       if (deletedLastTag) {
-        editedItem.querySelector(".new-tag").focus()
+        // Focus the input field to add new tags
+        getNewTagField(ev.target).focus()
       } else {
         if (focusedTagIndex === 0) {
           // we deleted tag 0 so we'll give focus to next one, aka tag 1 (which is the new item at index 0)
