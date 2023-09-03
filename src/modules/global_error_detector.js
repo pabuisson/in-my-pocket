4c0501f6bf904c246021559b250c953a6237ee93
Pierre-Adrien <pabuisson@gmail.com>
2023-09-03 21:43:14 +0200

    only trigger the first notification after 10 errors

--- src/modules/global_error_detector.js
+++ src/modules/global_error_detector.js
@@ -4,69 +4,115 @@ import Settings from "./settings"
 import Logger from "./logger"
 import { NotificationIds } from "./constants"
 
+// TODO: don't store a unique object, but use "namespaced" keys such as errors:countByDay, errors:lastNotifiedOn
+// TODO: cleanup local storage errors data when the bug report is enabled (or next time it passes in onError
+//        with bug report enabled)
 const GlobalErrorDetector = (function () {
-  async function shouldAskToEnable() {
-    // Early return if the error tracking has already been enabled
+  async function alreadyEnabledBugReporting() {
     await Settings.init()
     const bugReportEnabled = await Settings.get("bugReport")
-    if (bugReportEnabled) return false
 
-    // TODO: early return if the user said they didn't want to enable bug reporting
+    return bugReportEnabled
+  }
 
-    return true
+  async function incrementTodaysErrorCount() {
+    const today = new Date()
+    const todayIso = today.toISOString().slice(0, 10)
+
+    const { GlobalErrorDetector: errorData = { countByDay: {}, lastNotifiedOn: null } } =
+      await browser.storage.local.get("GlobalErrorDetector")
+
+    if (!errorData.countByDay[todayIso]) {
+      errorData.countByDay[todayIso] = 1
+    } else {
+      errorData.countByDay[todayIso] += 1
+    }
+
+    // TODO: clean up items older than the max days to consider
+
+    await browser.storage.local.set({ GlobalErrorDetector: errorData })
+
+    return
+  }
+
+  const ERRORS_COUNT_AFTER_WHICH_NOTIFY = 10
+
+  // - no "previous notification at" & count >= 10
+  // - TODO: previous notification at > 3.months.ago & count >= 100
+  async function shouldNotify() {
+    const {
+      GlobalErrorDetector: { countByDay, lastNotifiedOn },
+    } = await browser.storage.local.get("GlobalErrorDetector")
+
+    const relevantErrorsCount = Object.keys(countByDay).reduce((total, isoDateKey) => total + countByDay[isoDateKey], 0)
+    console.log({ relevantErrorsCount })
+    return !lastNotifiedOn && relevantErrorsCount > ERRORS_COUNT_AFTER_WHICH_NOTIFY
+  }
+
+  async function updateLastNotifiedOn() {
+    const { GlobalErrorDetector } = await browser.storage.local.get("GlobalErrorDetector")
+    GlobalErrorDetector.lastNotifiedOn = new Date().toISOString().slice(0, 10)
+
+    browser.storage.local.set({ GlobalErrorDetector })
+  }
+
+  // FIXME: very buggy. If I don't click on the notification straight away, it looks like it has no event
+  //        handling attached anymore, at least if the error is triggered from the popup
+  function notify() {
+    const title = "In My Pocket: an error occurred"
+    const message = "It's OK. If you want to help, you can enable anonymous bug reports in the settings"
+    const contextMessage = "It will anonymously report errors to me, so that I can identify them more easily"
+
+    // NOTE: a "buttons" option exists but is not implemented by Firefox/Opera/Safari
+    //       I could use it for Chrome though
+    // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/notifications/NotificationOptions
+    browser.notifications.create(NotificationIds.AN_ERROR_OCCURRED, {
+      type: "basic",
+      // TODO: iconUrl
+      title,
+      message,
+      contextMessage,
+    })
+
+    browser.notifications.onClicked.addListener(notificationId => {
+      if (notificationId == NotificationIds.AN_ERROR_OCCURRED) {
+        browser.notifications.clear(notificationId)
+        browser.runtime.openOptionsPage().then(() => {
+          setTimeout(() => {
+            const event = { action: "settings:highlight", section: "bug-report" }
+            browser.runtime.sendMessage(event)
+          }, 500)
+        })
+      }
+    })
   }
 
   return {
     init: async () => {
       if (!window) {
-        Logger.warn("(GlobalErrorDetector) Window not available in this context")
+        Logger.warn("(GlobalErrorDetector) window is not available in the current context")
         return
       }
 
-      if (!shouldAskToEnable()) {
-        Logger.warn("(GlobalErrorDetector) The user should not be asked to enable the bug reporting")
+      if (await alreadyEnabledBugReporting()) {
+        Logger.log("(GlobalErrorDetector) The user already enabled error reporting")
         return
       }
 
-      window.onerror = function () {
-        const title = "In My Pocket: an error occurred"
-        const message = "It's OK. If you want to help, you can enable anonymous bug reports in the settings"
-        const contextMessage = "It will anonymously report errors to me, so that I can identify them more easily"
+      window.onerror = async function () {
+        console.error("CAUTH AN ERROR")
+        await incrementTodaysErrorCount()
 
-        // NOTE: a "buttons" option exists but is not implemented by Firefox/Opera/Safari
-        //       I could use it for Chrome though
-        // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/notifications/NotificationOptions
-        browser.notifications.create(NotificationIds.AN_ERROR_OCCURRED, {
-          type: "basic",
-          // TODO: iconUrl
-          title,
-          message,
-          contextMessage,
-        })
-
-        // NOTE: very buggy. If I don't click on the notification straight away, it looks like it has no event
-        //        handling attached anymore, at least if the error is triggered from the popup
-        browser.notifications.onClicked.addListener(notificationId => {
-          if (notificationId == NotificationIds.AN_ERROR_OCCURRED) {
-            browser.notifications.clear(notificationId)
-            browser.runtime.openOptionsPage().then(() => {
-              setTimeout(() => {
-                const event = { action: "settings:highlight", section: "bug-report" }
-                browser.runtime.sendMessage(event)
-              }, 500)
-            })
-          }
-        })
-
-        // Store the error information (if needed?)
-        // And ask the user if they want to enable bug reporting
-        // If they don't: store the fact that they refused to avoid spamming them
-        // NOTE: I can store either the date and say I won't ask again for x months
-        //       or store the version and say I won't ask again until the next minor version
+        if (await shouldNotify()) {
+          updateLastNotifiedOn()
+          notify()
+        }
 
         // Prevent the default browser error handling
         return true
       }
+
+      Logger.log("(GlobalErrorDetector) onerror event listener has been setup!")
     },
   }
 })()
