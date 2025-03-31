d3c7fb38e6bdf65cd490ba08653a3629a117e008
Pierre-Adrien Buisson <pabuisson@gmail.com>
2025-03-31 21:37:18 +0200

    fix(tests): stub getManifest for the test suite

--- jest.setup.js
+++ jest.setup.js
@@ -10,3 +10,8 @@ if (typeof self.crypto === "undefined") {
   const { randomUUID } = require("crypto") // Use Node.js crypto module
   self.crypto = { randomUUID }
 }
+
+// Stub the default return value of getManifest to allow Environment.isDevelopment()
+// to run smoothly in the test environment (and without having to stub it for every
+// single test that indirectly end up calling Environment)
+chrome.runtime.getManifest = jest.fn().mockReturnValue({ version: "1.2.3" })
